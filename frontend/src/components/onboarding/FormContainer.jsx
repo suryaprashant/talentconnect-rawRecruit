057436c66a143f07d59cfc74e834339707a3e18b
Sohard Begraj <140617987+Sohardbegraj@users.noreply.github.com>
2025-09-25 01:28:17 +0530

    Delete frontend directory

--- frontend/src/components/onboarding/FormContainer.jsx
+++
@@ -1,9 +0,0 @@
-function FormContainer({ children }) {
-    return (
-      <div className="w-full max-w-2xl mx-auto bg-white rounded-lg shadow-lg overflow-hidden animate-fade-in">
-        {children}
-      </div>
-    )
-  }
-  
-  export default FormContainer
