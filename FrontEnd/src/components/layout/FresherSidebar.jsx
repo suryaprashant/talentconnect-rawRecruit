8388a965ecbe6379b92bbadbe24ebd859c77675e
Prashu Verma <prashuverma012@gmail.com>
2025-09-25 14:35:43 +0530

    student sidebar link issue fixed

--- FrontEnd/src/components/layout/FresherSidebar.jsx
+++ FrontEnd/src/components/layout/FresherSidebar.jsx
@@ -84,16 +84,16 @@ function FresherSidebar({ activePath }) {
 
       <SidebarNavGroup label="Application Status" icon={<FiCheckCircle />} active={activePath.includes('/application-status')}>
         <SidebarNavItem 
-          to="/application-status/job-listing" 
+          to="/application-status/Job-listing" 
           icon={<FiBriefcase />} 
           label="Job Listing" 
-          active={activePath === '/application-status/job-listing'} 
+          active={activePath === '/application-status/Job-listing'} 
         />
         <SidebarNavItem 
-          to="/application-status/off-campus-listing" 
+          to="/application-status/Off-campus" 
           icon={<FiLayers />} 
           label="Off-Campus Listing" 
-          active={activePath === '/application-status/off-campus-listing'} 
+          active={activePath === '/application-status/Off-campus'} 
         />
         <SidebarNavItem 
           to="/application-status/Internship" 
