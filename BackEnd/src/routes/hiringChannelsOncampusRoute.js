6f956bfc53be6941e1ab2fa04c07ffbd32494681
Prashu Verma <prashuverma012@gmail.com>
2025-09-30 21:19:08 +0530

    refactoring backend architecture

--- BackEnd/src/routes/hiringChannelsOncampusRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { submitRequest } from "../controllers/hiringChannelsOncampusController.js";
-
-const router = express.Router();
-
-// GET /api/applications
-router.post("/oncampus", submitRequest);
-
-export default router;
