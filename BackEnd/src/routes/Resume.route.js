d4c56330f282256069c78030cd16524687c98219
NirdeshKajiShakya <nirkajishakya23@gmail.com>
2025-10-06 23:01:05 +0545

    attempting to fix naming errors

--- BackEnd/src/routes/Resume.route.js
+++ BackEnd/src/routes/Resume.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import OnboardingModel from "../models/studentonboardingmodel.js";
+import OnboardingModel from "../models/studentonboardingModel.js";
 
 const router = express.Router();
 
