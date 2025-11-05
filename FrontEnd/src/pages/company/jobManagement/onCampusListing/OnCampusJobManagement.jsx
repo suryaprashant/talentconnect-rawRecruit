df369b79facaf7f5578b5de689c03431e5d743f5
Prashu Verma <prashuverma012@gmail.com>
2025-11-05 16:34:15 +0530

    job desc view and view application issue correction

--- FrontEnd/src/pages/company/jobManagement/onCampusListing/OnCampusJobManagement.jsx
+++ FrontEnd/src/pages/company/jobManagement/onCampusListing/OnCampusJobManagement.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { Search, Eye, ChevronLeft, ChevronRight, Trash } from 'lucide-react';
 // import axios from 'axios';
 import CollegeRequestDetail from './CollegeRequestDetail';
+import { Link } from 'react-router-dom'
 import { acceptCandidate, deleteJobById, getCollegeApplicationsForJob, getPostedJobs, rejectCandidate, shortlistCandidate } from '@/lib/Company_AxiosInstance';
 import toast from 'react-hot-toast';
 // import { format, isValid } from 'date-fns';
@@ -46,7 +47,7 @@ export default function OnCampusJobManagement() {
     setCollegesLoading(true);
     setError(null);
     try {
-      const response = await getCollegeApplicationsForJob(jobId, jobType,"Applied")
+      const response = await getCollegeApplicationsForJob(jobId, jobType, "Applied")
       // console.log("College: ", response);
       setColleges(response.data);
     } catch (err) {
@@ -277,6 +278,14 @@ export default function OnCampusJobManagement() {
                           >
                             <Eye size={18} />
                           </button>
+                          <Link
+                            to={`/company-dashboard/On-campus/${job._id}?isApplied=true`}
+                            disabled={job.applicationCount === 0}
+                            className="text-gray-500 hover:text-blue-600 p-1 rounded-md hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed"
+                            title="View Job Description"
+                          >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-search-corner-icon lucide-file-search-corner"><path d="M11.1 22H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h8a2.4 2.4 0 0 1 1.706.706l3.589 3.588A2.4 2.4 0 0 1 20 8v3.25" /><path d="M14 2v5a1 1 0 0 0 1 1h5" /><path d="m21 22-2.88-2.88" /><circle cx="16" cy="17" r="3" /></svg>
+                          </Link>
                           <button onClick={() => handleDelete(job._id)} className="text-gray-500 hover:text-gray-700" title="Delete Job">
                             <Trash size={18} />
                           </button>
