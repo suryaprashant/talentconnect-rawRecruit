9699cb3ed6e9c569ebfc6bf81a793e6359bb3889
prathmesh ladkat <prathameshladkat336@gmail.com>
2025-12-18 10:48:13 +0530

    route changes

--- FrontEnd/src/pages/auth/SignupPage.jsx
+++ FrontEnd/src/pages/auth/SignupPage.jsx
@@ -428,7 +428,7 @@ const ONBOARDING_ROUTES = {
 
 const DASHBOARD_ROUTES = {
   student: '/home',
-  fresher: '/fresherhome',
+  fresher: '/home',
   professional: '/Profhome',
   company: '/home',
   college: '/home',
