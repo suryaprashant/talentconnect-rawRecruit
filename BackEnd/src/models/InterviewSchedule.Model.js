c79b7d9a74c1e19ebfa69a72a4f7848ccb5781e0
Prathmesh Ladkat <prathameshladkat336@gmail.com>
2026-01-23 13:56:07 +0530

    notification sending, routing

--- BackEnd/src/models/InterviewSchedule.Model.js
+++ BackEnd/src/models/InterviewSchedule.Model.js
@@ -10,7 +10,7 @@ const interviewScheduleSchema = new mongoose.Schema(
 
     jobType: {
       type: String,
-      enum: ["On-campus", "Off-campus", "Pool-campus"],
+      enum: ["On-campus", "Off-campus", "Pool-campus", "Internship"],
       required: true,
     },
 
