34789bb3b312ca258ad352543c4e167b85c645d2
Your Name <you@example.com>
2026-04-01 09:41:24 +0530

    no of new application for professional

--- BackEnd/src/routes/CandidateRoute.js
+++ BackEnd/src/routes/CandidateRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import secureRoute from "../middlewares/secureRouteMiddleware.js";
-import { getAlumniPostedJobs,ProfileScore } from '../controllers/AlumniJobsController.js';
+import { getAlumniPostedJobs,ProfileScore,getNewApplications} from '../controllers/AlumniJobsController.js';
 const router = express.Router();
 
 router.get('/alumni',secureRoute,getAlumniPostedJobs)
 router.get('/profile-score',secureRoute,ProfileScore)
+router.get('/new-application',secureRoute,getNewApplications)
 
 export default router;
