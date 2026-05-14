c170e6a1c34d58597b0eb86650e32706694e2a8e
Your Name <you@example.com>
2026-05-14 15:51:43 +0530

    new user api

--- BackEnd/src/routes/CandidateRoute.js
+++ BackEnd/src/routes/CandidateRoute.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import secureRoute from "../middlewares/secureRouteMiddleware.js";
 //import { getAlumniPostedJobs,ProfileScore,getNewApplications} from '../controllers/AlumniJobsController.js';
-import { getAlumniWhoCanHelp ,getAlumniHiringNetwork, ProfileScore, getNewApplications, getCollegeAlumni, getCompanyAlumni } from '../controllers/AlumniJobsController.js';
+import { getAlumniWhoCanHelp ,getAlumniHiringNetwork, ProfileScore, getNewApplications, getCollegeAlumni, getCompanyAlumni,getNewUser } from '../controllers/AlumniJobsController.js';
+import verifyUser from '../middlewares/verifyUser.js';
 const router = express.Router();
 
 router.get('/college-alumni', secureRoute, getCollegeAlumni);
@@ -10,5 +11,6 @@ router.get('/alumni/:company/:postedByUser', secureRoute, getAlumniWhoCanHelp);
 router.get('/profile-score',secureRoute,ProfileScore)
 router.get('/new-application',secureRoute,getNewApplications)
 router.get('/hiring-network',secureRoute,getAlumniHiringNetwork)
+router.get('/new-users',verifyUser,getNewUser)
 
 export default router;
