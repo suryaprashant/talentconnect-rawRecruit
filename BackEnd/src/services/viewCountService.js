6f7c3340601a60e96774f5612487f983ef1a0d9b
kanishqtalentconnect-prog <kanishq.talentconnect@gmail.com>
2026-05-15 11:39:17 +0530

    Redis connection - Kanishq

--- BackEnd/src/services/viewCountService.js
+++ BackEnd/src/services/viewCountService.js
@@ -1,48 +1,29 @@
-import redis from 'redis';
-import { JobPostingTable } from '../models/jobPostingsModel.js'
-
-const redisHost = process.env.REDIS_HOST;
-const redisPort = process.env.REDIS_PORT;
-const redisPassword = process.env.REDIS_PASSWORD;
-
-const client = redis.createClient({
-    socket: {
-        host: redisHost,
-        port: redisPort,
-    },
-    password: redisPassword,
-});
-
-client.on('error', (err) => {
-    console.error('Redis Client Error', err);
-});
-
-async function connectRedis() {
-    if (!client.isOpen) {
-        await client.connect();
-        console.log("connected with redis")
-    }
-}
-
-connectRedis().catch(console.error);
+import { redisConnection } from "../common/redis.js";
+import { JobPostingTable } from "../models/jobPostingsModel.js";
 
 export async function viewCountService(jobId, viewerId) {
     console.log("called");
+
     if (!viewerId) {
-        throw new Error('No viewerId provided');
+        throw new Error("No viewerId provided");
     }
 
-    await connectRedis();
     console.log(jobId, viewerId);
+
     const redisKey = `viewed:${jobId}:${viewerId}`;
 
-    const alreadyViewed = await client.exists(redisKey);
+    const alreadyViewed = await redisConnection.exists(redisKey);
+
     if (alreadyViewed) {
         return false;
     }
 
     // mark viewed - 7 days expiry
-    await client.set(redisKey, '1', { EX: 604800 });
-    await JobPostingTable.findByIdAndUpdate(jobId, { $inc: { views: 1 } });
+    await redisConnection.set(redisKey, "1", "EX", 604800);
+
+    await JobPostingTable.findByIdAndUpdate(jobId, {
+        $inc: { views: 1 },
+    });
+
     return true;
 }
