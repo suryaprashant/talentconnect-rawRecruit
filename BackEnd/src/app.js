e8c39f02fc866fff31a2233edf4c31e578907923
kanishqtalentconnect-prog <kanishq.talentconnect@gmail.com>
2026-06-13 19:50:33 +0530

    fix 1.1

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+dotenv.config(); 
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -17,7 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Load environment variables - This must be the first logic that needs env vars
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+// dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 // dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const PORT = process.env.PORT || 5000;
